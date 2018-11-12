7ac200f527f4aa86a13d59b6f4f467b06bb3b4b3
ART LAB <artasalab@gmail.com>
2018-11-11 18:33:40 -0600

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -41,3 +41,15 @@ if(!localStorage.getItem('name')) {
 myButton.onclick = function() {
   setUserName();
 }
+
+
+var myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    var mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/logo.png') {
+      myImage.setAttribute ('src','images/logo2.png');
+    } else {
+      myImage.setAttribute ('src','images/logo.png');
+    }
+}
